572425db83122c7ff3f4e9156c87b76792af1dfe
AdiPat <contact.adityapatange@gmail.com>
2022-10-02 10:49:23 +0530

    pages: add landing, create and event page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
+import "./App.css";
+import { LandingPage, CreatePage, EventPage } from "./pages";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <LandingPage />,
+  },
+  {
+    path: "/create",
+    element: <CreatePage />,
+  },
+  {
+    path: "/event",
+    element: <EventPage />,
+  },
+]);
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ src/pages/index.js
@@ -0,0 +1,5 @@
+import { LandingPage } from "./landing-page";
+import { CreatePage } from "./create-page";
+import { EventPage } from "./event-page";
+
+export { LandingPage, CreatePage, EventPage };
